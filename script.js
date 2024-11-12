4aa9465ab8660fcabd8af7ffe37d977715e0ce8d
Arthur Godwin <164368034+arthurgodwinmaia@users.noreply.github.com>
2024-11-12 00:58:21 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,26 +1,4 @@
-// ==UserScript==
-// @name         Alura Destroyer
-// @namespace    https://cursos.alura.com.br/
-// @version      2024-09-23
-// @description  nuh uh i dont want to do alura nomore :sob:
-// @author       wwwwwwwwwww
-// @match        https://cursos.alura.com.br/course/*/task/*
-// @icon         https://i.imgur.com/gP1LZq9.png
-// @grant        none
-// ==/UserScript==
-
-(function() {
-    'use strict';
-    const water_mark = document.querySelector('.formattedText');
-    water_mark.innerHTML = 'sussy baka amongus';
-
-    let cookies = document.cookie;
-
-    let actual_url = window.location.href;
-
-    let next_lesson_button = document.getElementsByClassName("bootcamp-next-button")[0];
-
-    if (next_lesson_button){
+if (next_lesson_button){
         let next_lesson_link = next_lesson_button.getAttribute('href');
         let parts = actual_url.split('/');
         let lessonName = parts[4];
@@ -36,9 +14,3 @@
           }}).then(data => {
           console.log("[DEBUG] Lesson Done!")
           })
-
-        setTimeout(next_lesson_button.click(), 4000); // Modificar o tempo para 4 segundos
-    } else {
-        alert("Next Lesson Button not found :( are u sure that u are on the correct page?");
-    }
-})();
